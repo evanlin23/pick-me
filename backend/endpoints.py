d37eafaf36b24e6b5339c3adc099170c94ef5162
davidhan48 <21handl1@kgv.hk>
2024-02-22 11:07:19 -0600

    added utils endpoint

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
+@app.route('/utils')
+def hello_utils():
+    return 'Hello utils'
+
 if __name__ == '__main__':
     app.run()
 
