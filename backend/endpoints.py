6977e440a04339b01351fe9b87614d6c44d36fe0
lrhyde <lrhyde13@gmail.com>
2024-02-21 22:29:57 -0600

    add jest config and endpoint test

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World'
 
+@app.route('/test', methods=['GET'])
+def test():
+    return "Hello"
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8000)
 
