4dd3809b3277a60af71a7afe48fd868475adaae0
lrhyde <lrhyde13@gmail.com>
2024-02-27 19:04:36 -0600

    nextui and router config

---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import {Button, Input, Slider} from "@nextui-org/react";
+export const Login = () =>{
+    return(
+        <div>
+            <h1>Log in and select your preferences</h1>
+            <Input/>
+            <Slider/>
+            <Slider/>
+            <Button/>
+        </div>
+    )
+}
