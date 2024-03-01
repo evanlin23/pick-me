108da0782a0ef082b70e0027a3af6979743ee5ba
evanlin23 <evanlin366@gmail.com>
2024-02-29 21:11:22 -0600

    Update the colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ const { nextui } = require("@nextui-org/react");
 export default {
   content: ["./src/*.tsx", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      turquoise: "#7FB5B5"
+    }
     extend: {},
   },
   plugins: [nextui()],
