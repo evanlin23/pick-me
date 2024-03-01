1e3ff6c5458108967bad50680f9af3e11f7b8b9a
evanlin23 <evanlin366@gmail.com>
2024-02-29 21:08:27 -0600

    Change something I forgot

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -1,14 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World'
 
-@app.route('/test', methods=['GET'])
-def test():
-    return "Hello"
-
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run()
 
